2d659535b0b3ebbb9fb4201210e7d1cc51a30b7c
Leonardo Montoya <leonardo.montoya@utec.edu.pe>
2025-04-06 00:11:55 -0500

    Creacion de todos los metodos de LinkedList

--- include/eStl/eLinkedList/eLinkedList.h
+++ include/eStl/eLinkedList/eLinkedList.h
@@ -9,85 +9,33 @@
 #include "Node.h"
 template<typename T>
 class eLinkedList {
-public:
     Node<T> *head;
-    Node<T> *tail;
+public:
     eLinkedList();
     ~eLinkedList();
-    T front();
-    T back();
+
+    [[nodiscard]] bool empty() const;
+    [[nodiscard]] int size() const;
+
     void push_front(T value);
     void push_back(T value);
-
+    T front();
+    T back();
+    T pop_front();
+    T pop_back();
+    T operator[](int);
+    int size();
+    void clear();
+    Node<T> *getMiddle(Node<T>* start);
+    Node<T> *merge(Node<T> *left, Node<T> *right);
+    Node<T> *mergeSort(Node<T> *start);
+    void sort();
+    void reverse();
     void show();
 };
 
-template<typename T>
-eLinkedList<T>::eLinkedList() {
-    head = nullptr;
-    tail = nullptr;
-}
-
-template<typename T>
-eLinkedList<T>::~eLinkedList() {
-    Node<T> *current = head;
-    while (current != nullptr) {
-        Node<T> *next = current->next;
-        delete current;
-        current = next;
-    }
-}
-
-template<typename T>
-T eLinkedList<T>::front() {
-    if (head != nullptr) {
-        return head->val;
-    }
-    throw std::out_of_range("List is empty");
-}
-
-template<typename T>
-T eLinkedList<T>::back() {
-    if (tail != nullptr) {
-        return tail->val;
-    }
-    throw std::out_of_range("List is empty");
-}
-template<typename T>
-void eLinkedList<T>::push_front(T value) {
-    auto *newNode = new Node<T>;
-    newNode->val = value;
-    newNode->next = head;
-    head = newNode;
-    if (tail == nullptr) {
-        tail = newNode;
-    }
-
-}
-
-template<typename T>
-void eLinkedList<T>::push_back(T value) {
-    auto *newNode = new Node<T>;
-    newNode->val = value;
-    newNode->next = nullptr;
-    if (tail != nullptr) {
-        tail->next = newNode;
-    }
-    tail = newNode;
-    if (head == nullptr) {
-        head = newNode;
-    }
-}
-
-template<typename T>
-void eLinkedList<T>::show() {
-    Node<T> *current = head;
-    while (current != nullptr) {
-        std::cout << current->val << " ";
-        current = current->next;
-    }
-    std::cout << std::endl;
-}
 
 
+
+#include "../src/eLinkedList/eLinkedList.tpp"
 #endif //ESTL_ELINKEDLIST_H
